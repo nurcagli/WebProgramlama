067c9e885d527c29b891783203f07a0b45eb148b
Rabia Nur Çağlı <74273089+nurcagli@users.noreply.github.com>
2022-08-14 01:11:54 +0300

    migration added

---
+++ WebProgramlamaProje/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.Data
+{
+    public class ApplicationDbContext: IdentityDbContext // veritabanı adı ne olmalı?
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) //connectionstring bilgisini dışarıdan alıyoruz.
+          : base(options)
+        {
+            //options.UseSqlite("DataSource=webProgramlama.db");
+        }
+
+        // buraya tabloları eklıyoruz.
+        public DbSet<Work> Works { get; set; }
+        public DbSet<InteriorWork> InteriorWorks { get; set; }
+        public DbSet<OutdoorWork> OutdoorWorks { get; set; }
+        public DbSet<FurnitureWork> FurnitureWorks { get; set; }
+
+    }
+}
--- WebProgramlamaProje/Startup.cs
+++ WebProgramlamaProje/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebProgramlamaProje.Data;
 
 namespace WebProgramlamaProje
 {
@@ -23,6 +26,15 @@ namespace WebProgramlamaProje
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(
+                  Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+
             services.AddControllersWithViews();
         }
 
